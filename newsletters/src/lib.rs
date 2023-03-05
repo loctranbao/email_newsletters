cf26f6e87f1ef77944806909a7e852b9d5515e05
chenlog <loc.tranbao@outlook.com>
2023-03-05 16:39:33 +0700

    basic health_check implementaion and testing

---
+++ newsletters/src/lib.rs
@@ -0,0 +1,26 @@
+use std::net::TcpListener;
+
+use actix_web::{web, HttpServer, App, HttpRequest, Responder, HttpResponse, dev::Server};
+
+/*
+    handler to check if our web server still healthy or not
+    simply by return Http 200 OK code
+ */
+async fn health_check(_req: HttpRequest) -> impl Responder {
+    HttpResponse::Ok().finish()
+}
+
+/*
+    Create http web server with contain an app
+    to handle Http requests parser, routine to request handler
+*/
+pub fn run(listener : TcpListener) -> std::io::Result<Server> {
+    let server = HttpServer::new(|| {
+        App::new()
+            .route("/health_check", web::get().to(health_check))
+    })
+    .listen(listener)?
+    .run();
+
+    Ok(server)
+}
---
+++ newsletters/src/main.rs
@@ -0,0 +1,42 @@
+
+use std::net::TcpListener;
+
+use zero2prod::run;
+
+/*
+    asynchronous runtime for webserver
+    we use tokio to generate the run time for our webserver
+
+    what it does is simply generate code to wrap the code in the main
+    to an async block
+
+    then it generates and executor and put that async task to run
+    and wait for future correspondin the that task
+ */
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:8000")?;
+    run(listener)?.await
+}
+
+
+//  this is the code tokio generated behind the scene look like
+/*
+    fn main() -> std::io::Result<()> {
+        let body = async move {
+        HttpServer::new(|| {
+        App::new()
+        .route("/", web::get().to(greet))
+        .route("/{name}", web::get().to(greet))
+        })
+        .bind("127.0.0.1:8000")?
+        .run()
+        .await
+        };
+        tokio::runtime::Builder::new_multi_thread()
+        .enable_all()
+        .build()
+        .expect("Failed building the Runtime")
+        .block_on(body)
+        }
+ */
